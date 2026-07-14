a6b9b309a4e341edf4e9cedea0b405cefe677795
sktharunn-jpg <sktharunn@gmail.com>
2026-07-14 10:05:30 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.py
@@ -0,0 +1,11 @@
+# Last updated: 14/07/2026, 10:05:32
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        sum = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+                sum -= values[s[i]]
+            else:
+                sum += values[s[i]]
+        return sum
