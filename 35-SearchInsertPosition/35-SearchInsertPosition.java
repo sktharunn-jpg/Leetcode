46bac75069658edcd705b37d36e3a8f293ae4007
sktharunn-jpg <sktharunn@gmail.com>
2026-07-14 10:05:17 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,18 @@
+// Last updated: 14/07/2026, 10:05:19
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
