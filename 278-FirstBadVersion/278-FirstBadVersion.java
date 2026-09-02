25b3890a7b27696423ba40969a9fbd1c585a7fae
sktharunn-jpg <sktharunn@gmail.com>
2026-09-02 12:25:34 +0530

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.java
@@ -0,0 +1,12 @@
+// Last updated: 02/09/2026, 12:25:33
+1public class Solution extends VersionControl {
+2    public int firstBadVersion(int n) {
+3        int l=0,h=n;
+4        while(l<=h){
+5            int mid = l+(h-l)/2;
+6            if(isBadVersion(mid)) h = mid-1;
+7            else l = mid+1;
+8        }
+9        return l;
+10    }
+11}
